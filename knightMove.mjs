d6245cf792fa287e54f12cf2c714a86e1b528848
rib9985 <rib9985@gmail.com>
2024-08-28 15:11:26 -0300

    Create bfs method, add populateGraph method

--- knightMove.mjs
+++ knightMove.mjs
@@ -1,4 +1,9 @@
-export function knightMoves(initialPos, finalPos) {}
+export function knightMoves(initialPos, finalPos) {
+  const moves = new PopulateMoves(initialPos, finalPos);
+  const graph = moves.populateGraph();
+  const bfsResult = graph.bfs(finalPos, initialPos);
+  console.log(`The knight moves are: ${bfsResult}`);
+}
 
 const moves = [
   //first group of moves
@@ -14,26 +19,28 @@ const moves = [
 ];
 
 class PopulateMoves {
-  constructor() {
-    this.position = position;
+  constructor(initialPos, finalPos) {
+    this.initialPosition = initialPos;
+    this.position = this.initialPosition;
     this.previousPosition = null;
-    this.finalPosition = finalPosition;
+    this.finalPosition = finalPos;
   }
 
-  possibleMoves() {
+  getNextMoves() {
     const nextMoves = [];
 
-    moves.forEach((element) => {
-      const validMove = this.checkValidMove(element, this.position);
+    moves.forEach((move) => {
+      const validMove = this.checkValidMove(move, this.position);
 
       if (validMove) {
         nextMoves.push(validMove);
       }
     });
+    return nextMoves;
   }
 
   checkValidMove(position, move) {
-    finalPosition = [postion[0] + move[0], position[1] + move[1]];
+    const finalPosition = [position[0] + move[0], position[1] + move[1]];
     if (
       finalPosition[0] > -1 &&
       finalPosition[0] < 8 &&
@@ -43,19 +50,86 @@ class PopulateMoves {
       return finalPosition;
     }
   }
+
+  positionFound(move) {
+    if (move === this.finalPosition) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  createPath(graph, currentPosition, move) {
+    graph.addNode(move);
+    graph.addNode(currentPosition);
+    graph.addEdge(currentPosition, move);
+  }
+
+  populateGraph() {
+    const graph = new Graph();
+    const queue = [this.initialPosition];
+
+    while (queue.length > 0) {
+      this.position = queue.shift();
+
+      if (this.positionFound(this.position)) {
+        return graph;
+      } else {
+        const nextMoves = this.getNextMoves();
+
+        nextMoves.forEach((move) => {
+          if (!graph.nodes.has(move.toString()))
+            this.createPath(graph, this.position, move);
+          queue.push(move);
+        });
+      }
+    }
+  }
 }
 
 class Graph {
+  //uses map to structure an adjancency list
   constructor() {
     this.nodes = new Map();
   }
 
   addNode(node) {
-    this.nodes.set(node, []);
+    if (!this.nodes.has(node.toString())) {
+      this.nodes.set(node.toString(), []);
+    }
   }
 
   addEdge(source, destination) {
-    this.nodes.get(source).push(destination);
-    this.nodes.get(destination).push(source);
+    this.nodes.get(source.toString()).push(destination);
+    this.nodes.get(destination.toString()).push(source);
+  }
+
+  bfs(goal, root) {
+    const goalString = goal.toString();
+    const rootString = root.toString();
+    const queue = [[rootString]];
+    const explored = new Set();
+
+    explored.add(rootString);
+
+    while (queue.length > 0) {
+      const path = queue.shift();
+      const node = path[path.length - 1];
+
+      if (node === goal) {
+        return path.map((p) => p.split(",").map(Number));
+      }
+      const neighbors = this.nodes.get(node);
+
+      for (let neighbor of neighbors) {
+        const neighborString = neighbor.toString();
+        if (!explored.has(neighborString)) {
+          explored.add(neighborString);
+          const newPath = path.concat([neighborString]);
+          queue.push(newPath);
+        }
+      }
+    }
+    return null;
   }
 }
---
+++ knightTest.mjs
@@ -0,0 +1,4 @@
+import { knightMoves } from "./knightMove.mjs";
+
+knightMoves([3, 3], [0, 0]);
+knightMoves([0, 0], [3, 3]);
