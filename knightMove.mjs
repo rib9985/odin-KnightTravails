ac3afa2c5961ea6090deb54e00ae5ec533d2a86d
rib9985 <rib9985@gmail.com>
2024-08-29 17:45:02 -0300

    Remove classes, refactor to single bfs function

--- knightMove.mjs
+++ knightMove.mjs
@@ -1,135 +1,50 @@
 export function knightMoves(initialPos, finalPos) {
-  const moves = new PopulateMoves(initialPos, finalPos);
-  const graph = moves.populateGraph();
-  const bfsResult = graph.bfs(finalPos, initialPos);
-  console.log(`The knight moves are: ${bfsResult}`);
-}
+  const possibleMoves = [
+    //first group of moves
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2],
+    //second group of moves
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1],
+  ];
 
-const moves = [
-  //first group of moves
-  [1, 2],
-  [1, -2],
-  [-1, 2],
-  [-1, -2],
-  //second group of moves
-  [2, 1],
-  [2, -1],
-  [-2, 1],
-  [-2, -1],
-];
+  function bfs(start, goal) {
+    const queue = [[start]];
+    const visited = new Set();
 
-class PopulateMoves {
-  constructor(initialPos, finalPos) {
-    this.initialPosition = initialPos;
-    this.position = this.initialPosition;
-    this.previousPosition = null;
-    this.finalPosition = finalPos;
-  }
-
-  getNextMoves() {
-    const nextMoves = [];
-
-    moves.forEach((move) => {
-      const validMove = this.checkValidMove(move, this.position);
-
-      if (validMove) {
-        nextMoves.push(validMove);
-      }
-    });
-    return nextMoves;
-  }
-
-  checkValidMove(position, move) {
-    const finalPosition = [position[0] + move[0], position[1] + move[1]];
-    if (
-      finalPosition[0] > -1 &&
-      finalPosition[0] < 8 &&
-      finalPosition[1] > -1 &&
-      finalPosition[1] < 8
-    ) {
-      return finalPosition;
-    }
-  }
-
-  positionFound(move) {
-    if (move === this.finalPosition) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  createPath(graph, currentPosition, move) {
-    graph.addNode(move);
-    graph.addNode(currentPosition);
-    graph.addEdge(currentPosition, move);
-  }
-
-  populateGraph() {
-    const graph = new Graph();
-    const queue = [this.initialPosition];
-
-    while (queue.length > 0) {
-      this.position = queue.shift();
-
-      if (this.positionFound(this.position)) {
-        return graph;
-      } else {
-        const nextMoves = this.getNextMoves();
-
-        nextMoves.forEach((move) => {
-          if (!graph.nodes.has(move.toString()))
-            this.createPath(graph, this.position, move);
-          queue.push(move);
-        });
-      }
-    }
-  }
-}
-
-class Graph {
-  //uses map to structure an adjancency list
-  constructor() {
-    this.nodes = new Map();
-  }
-
-  addNode(node) {
-    if (!this.nodes.has(node.toString())) {
-      this.nodes.set(node.toString(), []);
-    }
-  }
-
-  addEdge(source, destination) {
-    this.nodes.get(source.toString()).push(destination);
-    this.nodes.get(destination.toString()).push(source);
-  }
-
-  bfs(goal, root) {
-    const goalString = goal.toString();
-    const rootString = root.toString();
-    const queue = [[rootString]];
-    const explored = new Set();
-
-    explored.add(rootString);
-
+    visited.add(start.toString());
     while (queue.length > 0) {
       const path = queue.shift();
-      const node = path[path.length - 1];
+      const currentPos = path[path.length - 1];
 
-      if (node === goal) {
-        return path.map((p) => p.split(",").map(Number));
+      if (currentPos.toString() === goal.toString()) {
+        return path;
       }
-      const neighbors = this.nodes.get(node);
-
-      for (let neighbor of neighbors) {
-        const neighborString = neighbor.toString();
-        if (!explored.has(neighborString)) {
-          explored.add(neighborString);
-          const newPath = path.concat([neighborString]);
-          queue.push(newPath);
+      for (const move of possibleMoves) {
+        const nextPos = [currentPos[0] + move[0], currentPos[1] + move[1]];
+        if (
+          nextPos[0] > -1 &&
+          nextPos[0] < 8 &&
+          nextPos[1] > -1 &&
+          nextPos[1] < 8 &&
+          !visited.has(nextPos.toString())
+        ) {
+          visited.add(nextPos.toString());
+          queue.push([...path, nextPos]);
         }
       }
     }
     return null;
   }
+
+  const result = bfs(initialPos, finalPos);
+  console.log(
+    `Knight goes from ${initialPos} -> ${finalPos} in ${result.length} moves, which are:`,
+  );
+  console.log(result);
+  return result;
 }
--- knightTest.mjs
+++ knightTest.mjs
@@ -1,4 +1,5 @@
 import { knightMoves } from "./knightMove.mjs";
 
+knightMoves([0, 0], [3, 3]); //expect [0,0] [1,2] [3,3] ;
 knightMoves([3, 3], [0, 0]);
-knightMoves([0, 0], [3, 3]);
+knightMoves([0, 0], [7, 7]);
